239c736de6831d4f7f328018b64c071cef6b0b02
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-03 22:28:51 +0530

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Concatenation
+{
+    public static void main(String[] args)
+    {
+        Scanner  sc=new  Scanner(System.in);
+        String a=sc.next();
+        System.out.println("Concatenation"+a+"you won 5 lakhs rupees");
+    }
+}
