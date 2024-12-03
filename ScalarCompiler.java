c23419d5ae031a4fd22fd50e852e736e8e24516f
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-03 22:40:28 +0530

    Add files via upload

---
+++ ScalarCompiler.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ScalarCompiler
+{
+    public static void main(String[] args)
+    {
+        Scanner  sc=new  Scanner(System.in);
+        String a=sc.next();
+        String b=String.format("congratulations %s",a);
+        System.out.println(b);
+    }
+}
