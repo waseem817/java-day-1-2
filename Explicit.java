f441defa27d125b1ecdae94d0e27aa2f9e8a0769
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-03 21:44:26 +0530

    Add files via upload

---
+++ Explicit.java
@@ -0,0 +1,12 @@
+public class Explicit
+{
+    public static void main(String[] args)
+    {
+        int a=50;
+        char b=(char)a;
+        char c='7';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
