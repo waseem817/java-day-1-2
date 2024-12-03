ba824fdb98e2e8caa065b02cb3dc8931f9e8fb9c
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-03 22:23:39 +0530

    Add files via upload

---
+++ AcceptingString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AcceptingString
+{
+    public static void main(String[] args)
+    {
+        Scanner  sc=new  Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
