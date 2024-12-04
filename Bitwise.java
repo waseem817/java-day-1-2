73888cdd934b7c39bce00010be2438f0b070a692
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-04 07:53:30 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+class Bitwise1 
+{
+    void Demo()
+    {
+    System.out.println(3&5);
+    System.out.println(3|5);
+    System.out.println(10&50);
+    System.out.println(10|50);
+    System.out.println(16&19);
+    System.out.println(16|19);
+    }
+}
+class Bitwise
+{
+    public static void main(String[] args)
+    {
+        Bitwise1 b=new Bitwise1();
+        b.Demo();
+    }
+}
